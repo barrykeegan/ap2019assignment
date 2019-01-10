d50ef85ecfac81a0d5890f6b6f80b38a01fe9d21
barrykeegan <barry.keegan@gmail.com>
2019-01-10 18:54:41 +0000

    Add my first calculator with no tests

---
+++ calc.py
@@ -0,0 +1,11 @@
+def add(first, second):
+    return first + second
+
+def subtract(first, second):
+    return first - second
+
+print(add(2+2))
+print(add(2+3))
+
+print(subtract(2,2))
+print(subtract(3,2))
